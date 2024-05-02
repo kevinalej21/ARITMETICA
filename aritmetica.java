11af332f455ea4aca227fbe26b7ba4c619a870b5
kevinalej21 <kevi.kc7@gmail.com>
2024-05-01 21:28:46 -0500

    Agregamos objetos

--- aritmetica.java
+++ aritmetica.java
@@ -1,7 +1,22 @@
 public class aritmetica {
 
-
+   // atributos de clase
     int a ;
     int b ;
+
+
+   // metodos
+    public void sumar (){
+
+    int resultado = a + b ;
+    System.out.println("resultado :"+resultado);
+    }
     
+    public int sumarConRetorno(){
+
+      //  int resultado = a + b ;
+      //  return resultado ;
+
+        return a + b ;
+    }
 }
---
+++ pruebaOperaciones.java
@@ -0,0 +1,20 @@
+public class pruebaOperaciones {
+
+    public static void main(String[] args) {
+
+        aritmetica aritmetica1 = new aritmetica();
+        aritmetica1.a = 3 ;
+        aritmetica1.b = 2 ;
+        aritmetica1.sumar();
+
+        int resultado = aritmetica1.sumarConRetorno() ; 
+
+        System.out.println("Resultado desde clase prueba : "+resultado);
+
+
+        
+    }
+    
+        
+    
+}
